b355292820c5179ff2afc70b7245587865d749d7
asimov <seyranmustafa4@gmail.com>
2024-09-05 18:27:57 +0300

    p

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -5,25 +5,29 @@ const TodoForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setValue(e.target.value)
   };
 
   return (
-    <form className="input-field" onSubmit={handleSubmit}>
-      <input
-        className="todo-input"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        type="text"
-        name="todo"
-        id="todo"
-        required
-        spellCheck="false"
-      />
-      <label> Enter your tasks</label>
-      <button type="submit" className="todo-btn">
-        Add New Task
-      </button>
-    </form>
+    <>
+      <form className="input-field" onSubmit={handleSubmit}>
+        <input
+          className="todo-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          type="text"
+          name="todo"
+          id="todo"
+          required
+          spellCheck="false"
+        />
+        <label> Enter your tasks</label>
+        <button type="submit" className="todo-btn">
+          Add New Task
+        </button>
+      </form>
+      <p>Task:{value}</p>
+    </>
   );
 };
 
