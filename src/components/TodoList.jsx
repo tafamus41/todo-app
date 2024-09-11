caf7194c3c3caa9a38bb81f469b67fd3427a0692
asimov <seyranmustafa4@gmail.com>
2024-09-11 18:55:55 +0300

    p

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { AiFillDelete } from "react-icons/ai";
+import { FaEdit } from "react-icons/fa";
 
-const TodoList = ({value,tasks}) => {
-    // console.log(value);
-    console.log(tasks);
+const TodoList = ({ value, tasks, setTasks }) => {
+  // console.log(value);
+  console.log(tasks);
   return (
-    <div></div>
-  )
-}
+    <div>
+      <p className="text-center ">
+        <AiFillDelete
+          type="button"
+          size={22}
+          className="text-danger cursor-pointer mx-3"
+          // onClick={() => deleteKitap(id)}
+        />
+        <FaEdit
+          data-bs-toggle="modal"
+          data-bs-target="#editModal"
+          size={20}
+          type="button"
+          className="me-2 text-warning cursor-pointer"
+          // onClick={() => setEditItem({title,ISBN,image,genre,id,author,publicationYear})}
+        />
+      </p>
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
