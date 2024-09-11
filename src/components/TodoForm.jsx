87450908f7b931474322209890758a62c46655f9
asimov <seyranmustafa4@gmail.com>
2024-09-11 17:39:24 +0300

    p

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TodoList from "./TodoList";
 
 const TodoForm = () => {
   const [value, setValue] = useState("");
@@ -10,30 +11,15 @@ const TodoForm = () => {
       setValue(""); 
     }
   };
-
   return (
     <>
       <form className="input-field" onSubmit={handleSubmit}>
-        <input
-          className="todo-input"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          type="text"
-          name="todo"
-          id="todo"
-          required
-          spellCheck="false"
-        />
+        <input className="todo-input" value={value} onChange={(e) => setValue(e.target.value)}
+          type="text" name="todo" id="todo" required spellCheck="false" />
         <label> Enter your tasks</label>
-        <button type="submit" className="todo-btn">
-          Add New Task
-        </button>
+        <button type="submit" className="todo-btn"> Add New Task </button>
       </form>
-      <ul className="list">
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
+      <TodoList/>
     </>
   );
 };
