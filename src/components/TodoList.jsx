06ead762bf14de0ff975fda14158c469da45c891
asimov <seyranmustafa4@gmail.com>
2024-09-13 18:18:55 +0300

    p

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,36 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { FaEdit } from "react-icons/fa";
 import EditTaskModal from "./EditTaskModal";
 
-const TodoList = ({setTasks,tasks,handleDelete }) => {
-  const[editTask,setEditTask]=useState("")
-    return (
-    
-    <div>
+const TodoList = ({ setTasks, tasks, handleDelete }) => {
+  const [editTask, setEditTask] = useState("");
+  return (
+    <div >
       {tasks.map((item, i) => (
-        <p
-          className="whattodo text-white border border-2 border-white p-2"
-          key={i}
-        >
-          {item}
-          <AiFillDelete
-            type="button"
-            size={22}
-            className="text-danger cursor-pointer mx-3"
-            onClick={() => handleDelete(i)}
-          />
-          <FaEdit
-            data-bs-toggle="modal"
-            data-bs-target="#editTaskModal"
-            size={20}
-            type="button"
-            className="me-2 text-danger cursor-pointer"
-            onClick={() => setEditTask(item)}
-          />
-        </p>
+        <div className="whattodo border border-2 border-white p-2 my-4">
+          <p
+            className="text-white my-2"
+            key={i}
+          >
+            {item}
+          </p>
+          <p>
+            <AiFillDelete
+              type="button"
+              size={22}
+              className="text-danger cursor-pointer mx-3"
+              onClick={() => handleDelete(i)}
+            />
+            <FaEdit
+              data-bs-toggle="modal"
+              data-bs-target="#editTaskModal"
+              size={20}
+              type="button"
+              className="me-2 text-danger cursor-pointer"
+              onClick={() => setEditTask(item)}
+            />
+          </p>
+        </div>
       ))}
-      <EditTaskModal editTask={editTask} setEditTask={setEditTask} tasks={tasks} setTasks={setTasks}/>
+      <EditTaskModal
+        editTask={editTask}
+        setEditTask={setEditTask}
+        tasks={tasks}
+        setTasks={setTasks}
+      />
     </div>
   );
 };
